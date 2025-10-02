d169478fec44b1f0b23cd5f74e4767342dac604c
Nadia Chornoyvan <nadia.chornoyvan@gmail.com>
2025-10-02 22:58:53 +0300

    commit task1.py

---
+++ Practice_4-5/task1.py
@@ -0,0 +1,17 @@
+#блок введення даних користувача
+user_name=input('Please enter your name: ') 
+user_last_name=input('Please enter your last name: ')
+phone_number=input('Please enter your phone number: ')
+street_name=input('Please enter your street name: ')
+house_number=input('Please enter your house number: ')
+apartment_number=input('Please enter your apartment number: ')
+city=input('Please enter your city: ')
+index=input('Please enter your index: ')
+country=input('Please enter your country: ')
+
+#блок виведення даних користувача
+print(user_name, user_last_name)
+print(phone_number)
+print('Str.',street_name,house_number,', ap.',apartment_number,',',city)
+print(index)
+print(country)
